af895f0729e6f0bf25f1f8d8be6e7bd7129a38e4
Your Name <victor.lopesvfk11@gmail.com>
2025-08-24 16:40:41 -0300

    Add update_at column to User model and implement migration scripts

--- fast_zero/fast_zero/models.py
+++ fast_zero/fast_zero/models.py
@@ -19,3 +19,4 @@ class User:
     created_at: Mapped[datetime] = mapped_column(
         init=False, server_default=func.now()
     )
+    update_at: Mapped[datetime] = mapped_column(onupdate=func.now())
--- fast_zero/tests/conftest.py
+++ fast_zero/tests/conftest.py
@@ -31,10 +31,18 @@ def _mock_db_time(*, model, time=datetime(2024, 1, 1)):
     def fake_time_hook(mapper, connection, target):
         if hasattr(target, 'created_at'):
             target.created_at = time
+        if hasattr(target, 'updated_at'):
+            target.updated_at = time
 
-    event.listen(model, 'before_insert', fake_time_hook)
+    def fake_update_time_hook(mapper, connection, target):
+        if hasattr(target, 'updated_at'):
+            target.updated_at = time
+
+    event.listen(model, 'before_insert', fake_time_hook)      
+    event.listen(model, 'before_update', fake_update_time_hook)
     yield time
     event.remove(model, 'before_insert', fake_time_hook)
+    event.remove(model, 'before_update', fake_update_time_hook)
 
 
 @pytest.fixture
